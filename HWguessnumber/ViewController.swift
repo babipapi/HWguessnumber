7c93a8368533e43b16a6c20795c2527c4922c16e
Daniel <babipapi@gmail.com>
2019-05-02 22:06:56 +0800

    fix1

--- HWguessnumber/ViewController.swift
+++ HWguessnumber/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
 
+    @IBOutlet weak var numberTextView: UITextView!
+    @IBOutlet weak var feedbackLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    var count = 6
+    var answer = Int.random(in: 0...50)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        feedbackLabel.text = "0到50猜一個數字，你有6次機會～"
+        timeLabel.text = "6"
     }
-
-
+    @IBAction func guess(_ sender: UIButton) {
+        var numbertext = Int(numberTextView.text!)
+        if numbertext != nil,numbertext == answer {
+            feedbackLabel.text = "你真棒，猜對了"
+           count = count - 1
+           timeLabel.text! = String(count)
+           
+        } else if numbertext != nil,numbertext! < answer {
+            feedbackLabel.text = "真可惜，在猜一次，數字在1到\(numbertext!)之間"
+            numberTextView.text = nil
+            count = count - 1
+            timeLabel.text! = String(count)
+        } else if numbertext != nil,numbertext! > answer {
+            feedbackLabel.text = "真可惜，在猜一次，數字在\(numbertext!)到50之間"
+            numberTextView.text = nil
+            count = count - 1
+            timeLabel.text! = String(count)
+        }
+        if count == 0 {
+            feedbackLabel.text = "哇哇哇，你已經沒機會了，再重猜一次吧"
+            numbertext = nil
+            count = 6
+    }
 }
-
+}
